75abfe7f39dec4ed4deda4285db884d38a1e9a5c
tat.bandelikova <tat.bandelikova@gmail.com>
2022-07-28 12:38:41 +0300

    popup

--- script.js/index.js
+++ script.js/index.js
@@ -1,4 +1,5 @@
 let popup_wrap = document.querySelector('.popup_wrap');
+let popup = document.querySelector('.popup');
 let btn_open = document.querySelector('.btn');
 let btn_close = document.querySelector('.popup_close');
 let body = document.querySelector('body');
@@ -13,12 +14,9 @@ btn_close.addEventListener('click', () => {
     body.classList.remove('no-scroll');
 });
 
-// window.addEventListener('click', (e) => {
-
-//     if(e.target.classList == 'popup_wrap') {
-//         return;
-//     } else {
-//         popup_wrap.style.display = 'none';
-//         body.classList.remove('no-scroll');
-//     }
-// })
+window.addEventListener('click', (e) => {
+    if(e.target == popup) {
+        popup_wrap.style.display = 'none';
+        body.classList.remove('no-scroll');
+    }
+});
